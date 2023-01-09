d78c9c65368c642df76825f4769c99eaef7019ea
chaipawn <chaiyapawn.n@gmail.com>
2023-01-09 21:00:52 +0700

    add loader component

--- client/src/pages/CampaignDetails.jsx
+++ client/src/pages/CampaignDetails.jsx
@@ -3,7 +3,7 @@ import { useLocation } from "react-router-dom";
 import { ethers } from "ethers";
 
 import { useStateContext } from "../context";
-import { CustomButton, CountBox } from "../components";
+import { CustomButton, CountBox, Loader } from "../components";
 import { calculateBarPercentage, daysLeft } from "../utils";
 import { thirdweb } from "../assets";
 
@@ -36,7 +36,7 @@ const CampaignDetails = () => {
 
   return (
     <div>
-      {isLoading && "Loading..."}
+      {isLoading && <Loader />}
 
       <div className="w-full flex md:flex-row flex-col mt-10 gap-[30px]">
         <div className="flex-1 flex flex-col">
